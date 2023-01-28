f5db208ae4fb6acd6f587227f28ea86a4795d2f5
Longs Pemun Gotar <developerlongs@gmail.com>
2023-01-28 23:44:43 +0000

    fixed some compile time errors

--- main.go
+++ main.go
@@ -1,105 +1,72 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-const (
-	nodeURL = "http://localhost:8545"
-	pollInterval = 60 * time.Second
-	logFile = "geth-performance.log"
-)
+const nodeURL = "https://mainnet.infura.io/v3/6ebc20d69d504a9c961be21a55cfe2e9"
+const logFile = "node_performance.log"
 
-type ethBlockNumber struct {
-	Jsonrpc string `json:"jsonrpc"`
-	Id int `json:"id"`
-	Result string `json:"result"`
-}
-
-type ethGasPrice struct {
-	Jsonrpc string `json:"jsonrpc"`
-	Id int `json:"id"`
-	Result string `json:"result"`
-}
-
-type netPeerCount struct {
-	Jsonrpc string `json:"jsonrpc"`
-	Id int `json:"id"`
-	Result string `json:"result"`
+type request struct {
+	JSONRPC string        `json:"jsonrpc"`
+	ID      string        `json:"id"`
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
 }
 
 func main() {
 	for {
 		// Get the current block number
-		blockNumber := getBlockNumber()
+		requestData := request{JSONRPC: "2.0", ID: "1", Method: "eth_blockNumber", Params: []interface{}{}}
+		requestBody := new(bytes.Buffer)
+		json.NewEncoder(requestBody).Encode(requestData)
+		response, _ := http.Post(nodeURL, "application/json", requestBody)
+
+		var result map[string]interface{}
+		json.NewDecoder(response.Body).Decode(&result)
+		blockNumber := result["result"].(string)
 
 		// Get the current gas price
-		gasPrice := getGasPrice()
+		requestData = request{JSONRPC: "2.0", ID: "1", Method: "eth_gasPrice", Params: []interface{}{}}
+		requestBody = new(bytes.Buffer)
+		json.NewEncoder(requestBody).Encode(requestData)
+		response, _ = http.Post(nodeURL, "application/json", requestBody)
 
-		// Get the current node peer count
-		peerCount := getPeerCount()
+		json.NewDecoder(response.Body).Decode(&result)
+		gasPrice := result["result"].(string)
 
-		// Log the performance data
+		// Get the current peer count
+		requestData = request{JSONRPC: "2.0", ID: "1", Method: "net_peerCount", Params: []interface{}{}}
+		requestBody = new(bytes.Buffer)
+		json.NewEncoder(requestBody).Encode(requestData)
+		response, _ = http.Post(nodeURL, "application/json", requestBody)
+
+		json.NewDecoder(response.Body).Decode(&result)
+		peerCount := result["result"].(string)
+
 		logPerformance(blockNumber, gasPrice, peerCount)
 
-		// Sleep for the specified interval
-		time.Sleep(pollInterval)
+		time.Sleep(60 * time.Second)
 	}
-}
-
-func getBlockNumber() string {
-	blockNumber := &ethBlockNumber{}
-	body := getJSONRPC("eth_blockNumber", []interface{}{})
-	json.Unmarshal(body, &blockNumber)
-	return blockNumber.Result
-}
-
-func getGasPrice() string {
-	gasPrice := &ethGasPrice{}
-	body := getJSONRPC("eth_gasPrice", []interface{}{})
-	json.Unmarshal(body, &gasPrice)
-	return gasPrice.Result
-}
-
-func getPeerCount() string {
-	peerCount := &netPeerCount{}
-	body := getJSONRPC("net_peerCount", []interface{}{})
-	json.Unmarshal(body, &peerCount)
-	return peerCount.Result
-}
-
-func getJSONRPC(method string, params []interface{}) []byte {
-	// Create the JSON-RPC request
-	request := map[string]interface{}{
-		"jsonrpc": "2.0",
-		"method": method,
-		"params": params,
-		"id": 1,
-	}
-
-	// Send the request
-	response, _ := http.Post(nodeURL, "application/json", json.NewEncoder(request))
-	body, _ := ioutil.ReadAll(response.Body)
-	return body
 }
 
 func logPerformance(blockNumber, gasPrice, peerCount string) {
-	// Log the performance data
-	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer f.Close()
+    // Log the performance data
+    f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    defer f.Close()
 
-	logString := fmt.Sprintf("Block number: %s, Gas price: %s, Peer count: %s, Timestamp: %s", blockNumber, gasPrice, peerCount, time.Now().String())
-	if _, err := f.WriteString(logString + "\n"); err != nil {
-		log.Println(err)
-	}
+    logString := fmt.Sprintf("Block number: %s, Gas price: %s, Peer count: %s, Timestamp: %s\n", blockNumber, gasPrice, peerCount, time.Now().String())
+    if _, err := f.WriteString(logString); err != nil {
+        log.Println(err)
+    }
 }
